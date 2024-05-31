edbffe2b7510a474c7429cbefa618a496dee9691
이호연 <ho991217@kakao.com>
2024-05-31 14:19:22 +0900

    feat: 필터 기능 추가

--- web-next/src/app/page.tsx
+++ web-next/src/app/page.tsx
@@ -2,8 +2,25 @@ import { Cam, Header, Tile } from '@components/home';
 import Image from 'next/image';
 import { FaMicrophone, FaPhoneFlip } from 'react-icons/fa6';
 import Demo from '../../public/images/demo.png';
+import Link from 'next/link';
 
-export default function HomePage() {
+type HomePageProps = {
+  searchParams: {
+    filter: '모든 카메라' | '거실' | '침실' | '주방';
+  };
+};
+
+export default function HomePage({
+  searchParams: { filter = '모든 카메라' },
+}: HomePageProps) {
+  const filterOptions = ['모든 카메라', '거실', '침실', '주방'];
+
+  const cams = [
+    <Cam key="거실" location='거실' name='거실 카메라' />,
+    <Cam key="주방" location='주방' name='주방 카메라' />,
+    <Cam key="침실" location='침실' name='침실 카메라' />,
+  ];
+
   return (
     <section className='w-full h-full flex flex-col items-center justify-between'>
       {/* <div className='w-full flex flex-col gap-4 mb-4'>
@@ -26,15 +43,21 @@ export default function HomePage() {
       </div> */}
       <div className='w-full flex flex-col'>
         <ul className='flex gap-4 font-medium text-lg mb-4'>
-          <li>모든 카메라</li>
-          <li className='text-muted-foreground'>거실</li>
-          <li className='text-muted-foreground'>침실</li>
-          <li className='text-muted-foreground'>주방</li>
+          {filterOptions.map((option) => (
+            <li
+              key={option}
+              className={`cursor-pointer ${
+                filter === option ? 'text-primary-500' : 'text-neutral-600'
+              }`}
+            >
+              <Link href={`/?filter=${option}`}>{option}</Link>
+            </li>
+          ))}
         </ul>
         <div className='w-full grid grid-cols-2 gap-2'>
-          <Cam location='거실' name='거실 카메라' />
-          <Cam location='주방' name='주방 카메라' />
-          <Cam location='침실' name='침실 카메라' />
+          {filter === '모든 카메라'
+            ? cams.map((cam) => cam)
+            : cams.filter((cam) => cam.props.location === filter)}
         </div>
       </div>
       <div className='w-full flex items-center gap-2 h-14'>
