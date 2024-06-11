a1e298330de2f507efb968696be080b8e1f9f4d0
이호연 <ho991217@kakao.com>
2024-06-11 17:01:17 +0900

    시;

--- host/package/send_images_to_ml_server.py
+++ host/package/send_images_to_ml_server.py
@@ -2,6 +2,7 @@ import cv2
 import time
 from uuid import uuid4
 import requests
+from package.stream import send_notification
 
 cap = cv2.VideoCapture(0)
 
@@ -24,12 +25,22 @@ def capture_and_send_images(fps:int = 10, duration:int = 10):
         # Clear images from memory
         del images
 
+
 def send_images_to_server(images):
+    url = "http://220.149.232.224:60001/predict"
+    files = [('images', (f'{uid}_frame_{i}.jpg', img, 'image/jpeg')) for uid, i, img in images]
+    payload = {}
+    headers = {}
+
     try:
-        url = "http://localhost:3001/upload"
-        files = [('images', (f'{uid}_frame_{i}.jpg', img, 'image/jpeg')) for uid, i, img in images]
-        response = requests.post(url, files=files)
-        print(response.status_code)
-    except Exception as e:
-        print(f"Failed to send images to server: {e}")
+        print("api request send")
+        response = requests.request("POST", url, headers=headers, data=payload, files=files).json()
+        did_fall = response["fall"]
+        id = response["id"]
         
+        if did_fall:
+            send_notification("낙상", "낙상이 감지되었습니다.", "test_user")
+            print('fall detected')
+        
+    except Exception as e:
+        print(f"{e}")
